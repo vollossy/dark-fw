597562c605dc33303536da39513ce00888b6142b
Konstantin.R.Dark <Konstantin.R.Dark@gmail.com>
2013-01-10 21:38:17 +0400

    Production + mini sandbox ( index_production )

--- www/index.php
+++ www/index.php
@@ -7,11 +7,9 @@ header('Content-Type: text/html; charset=utf-8');
     <head>
         <title>Sandbox</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<!--        <script type='text/javascript' src='lib/javascriptMVC/steal/steal.production.js?../../dark-fw/dark-bootstrap.js'></script>-->
         <script type='text/javascript' src='core/steal/steal.js?dark-fw/dark-fw.js'></script>
         <link rel="stylesheet" href="css/sandbox.css" type="text/css"/>
         <script type='text/javascript' src='js/prettify/prettify.js'></script>
-<!--        <script type="text/javascript" src="core/dark-fw/bootstrap.js"></script>-->
     </head>
     <body>
         <div>
